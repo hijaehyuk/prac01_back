ac7591ad13b4897e65c8836d217c16c658afcac5
hijaehyuk <messi300060@gmail.com>
2026-03-03 11:15:34 +0900

    [Feat] 카카오 소셜로그인 기능 구현

---
+++ src/main/java/com/example/demo/user/OAuth2UserService.java
@@ -0,0 +1,39 @@
+package com.example.demo.user;
+
+import com.example.demo.user.model.AuthUserDetails;
+import com.example.demo.user.model.User;
+import com.example.demo.user.model.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class OAuth2UserService extends DefaultOAuth2UserService {
+    private final UserRepository userRepository;
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        String provider = userRequest
+                .getClientRegistration().getRegistrationId();
+        // OAuth2 로그인 실행
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        UserDto.OAuth dto = UserDto.OAuth.from(oAuth2User.getAttributes(), "kakao");
+
+        // DB에 회원이 있나 없나 확인
+        Optional<User> result = userRepository.findByEmail(dto.getEmail());
+
+        // 없으면 가입 시켜주기
+        if (!result.isPresent()) {
+            User user = userRepository.save(dto.toEntity());
+            return AuthUserDetails.from(user);
+        } // 있으면 해당 사용자 반환
+        else {
+            User user = result.get();
+            return AuthUserDetails.from(user);        }
+    }
+}
